31d80036ad0cfc309da0bafb6f67428afec09156
Michael Wanderski <mike@wanderski.com>
2025-08-21 20:19:35 -0400

    seed mobile

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -2,16 +2,20 @@ import "dotenv/config";
 
 export default {
   expo: {
-    name: "SeedlingsMobile",
-    slug: "seedlings-mobile",
+    name: "seedlings3-mobile",
+    slug: "seedlings3-mobile",
     scheme: "seddlings",
     version: "1.0.0",
     orientation: "portrait",
     sdkVersion: "51.0.0",
     platforms: ["ios", "android"],
     updates: { enabled: false },
-    ios: { supportsTablet: true },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.seedlings.seedlings3",
+    },
     android: {
+      package: "com.seedlings.seedlings3",
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
