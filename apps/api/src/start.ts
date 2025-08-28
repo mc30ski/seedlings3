da1a5f5e84269284fccb89daa05140e50b6ef334
Michael Wanderski <mike@wanderski.com>
2025-08-28 18:17:51 -0400

    test

--- apps/api/src/start.ts
+++ apps/api/src/start.ts
@@ -4,9 +4,11 @@ try {
   const app = await buildApp();
   await app.ready();
 
-  app.get("/__routes", (_req, reply) =>
-    reply.type("text/plain").send(app.printRoutes())
-  );
+  if (process.env.ROUTE_DUMP === "1") {
+    app.get("/__routes", (_req, reply) =>
+      reply.type("text/plain").send(app.printRoutes())
+    );
+  }
 
   const port = Number(process.env.PORT) || 8080;
   await app.listen({ port, host: "0.0.0.0" });
