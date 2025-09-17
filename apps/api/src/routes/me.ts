1c24c692b4a353ed43968b2d05cbc7b59fb739e7
Michael Wanderski <mike@wanderski.com>
2025-09-16 21:10:44 -0400

    fix

--- apps/api/src/routes/me.ts
+++ apps/api/src/routes/me.ts
@@ -43,7 +43,7 @@ const clerk = CLERK_SECRET_KEY
 
 export default async function meRoutes(app: FastifyInstance) {
   app.get("/me", async (req, reply) => {
-    // 0) Env sanity
+    console.log("MIKEW: 2");
 
     if (!CLERK_SECRET_KEY) {
       app.log.error("[/me] CLERK_SECRET_KEY is not set");
