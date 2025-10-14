2f223576975e60ca8d05b4f5dd1a9c4134ab62fc
Michael Wanderski <mike@wanderski.com>
2025-10-14 16:15:20 -0400

    fixes

---
+++ apps/web/src/ui/tabs/Payments.tsx
@@ -0,0 +1,10 @@
+import ComingSoonNotice from "../notices/ComingSoonNotice";
+
+export default function Payments() {
+  return (
+    <ComingSoonNotice
+      title="Payments"
+      description="We’re building this soon. You’ll be able to see payments here."
+    />
+  );
+}
